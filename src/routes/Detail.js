f65cf8c4c41680c7c38d132dac25e32646cfe2af
Jinho Chun <jukoku@gmail.com>
2021-01-01 12:44:37 +0900

    Final Ver.

---
+++ src/routes/Detail.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      rating
+      description_intro
+      language
+      medium_cover_image
+      large_cover_image
+      isLiked @client
+    }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 80%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
+
+const Detail = () => {
+  let { id } = useParams();
+  id = parseInt(id);
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id },
+  });
+  return (
+    <Container>
+      <Column>
+        <Title>
+          {loading
+            ? "Loading..."
+            : `${data?.movie?.title}  ${data?.movie?.isLiked ? "❤" : "🙈"}`}
+        </Title>
+        <Subtitle>
+          {data?.movie?.language} · {data?.movie?.rating}
+        </Subtitle>
+        <Description>{data?.movie?.description_intro} </Description>
+      </Column>
+      <Poster bg={data?.movie?.large_cover_image}></Poster>
+    </Container>
+  );
+};
+
+export default Detail;
